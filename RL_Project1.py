5e2bbdb160f3d77d6e3d0a684061db187fbc5776
Andrew Ha <andrewha123123@gmail.com>
2023-01-03 13:28:01 -0800

    updates to RL_Project1, added functions to encapsulate the different tasks

--- RL_Project1.py
+++ RL_Project1.py
@@ -1,6 +1,6 @@
 import os
 import gym
-from stable_baselines3 import ppo
+from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
@@ -19,26 +19,43 @@ def file_setup():
         if (not os.path.exists(modelsPath)):
             os.mkdir(modelsPath)
 
-file_setup()
-environment_Name = "CartPole-v1"
-# env = gym.make(environment_Name)
-# print(env.action_space)
-# print(env.observation_space)
-# episodes = 10
-# for episode in range(1, episodes + 1):
-#     state = env.reset()
-#     done = False
-#     score = 0
-#     while (not done):
-#         env.render()
-#         action = env.action_space.sample()
-#         n_state, reward, done, info = env.step(action)
-#         score += reward
-#     print("Episode:{} Score:{}".format(episode, score))
-# env.close()
+def setup_environment(environment_name, debug=False):
+    environment_Name = "CartPole-v1"
+    env = gym.make(environment_Name)
+    if (debug):
+        print(env.action_space)
+        print(env.observation_space)
+        episodes = 10
+        for episode in range(1, episodes + 1):
+            state = env.reset()
+            done = False
+            score = 0
+            while (not done):
+                env.render()
+                action = env.action_space.sample()
+                n_state, reward, done, info = env.step(action)
+                score += reward
+            print("Episode:{} Score:{}".format(episode, score))
+        env.close()
+    return env
 
-log_path = "A:\VisualStudiosCode\PythonProjects\RL_Project\RL_Project1\Training\Logs"
+def setup_agent(environment_name, log_path):
+    env = setup_environment(environment_name)
+    env = DummyVecEnv([lambda: env])
+    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log = log_path)
+    return model
 
-env = gym.make(environment_Name)
-env = DummyVecEnv([lambda: env])
-# model = ppo('MlpPolicy', env, verbose=1, tensorboard_log = log_path)
+def save_model(environment_name):
+    
+
+def main():
+    environment_name = "CartPole-v1"
+    log_path = "A:\VisualStudiosCode\PythonProjects\RL_Project\RL_Project1\Training\Logs"
+    file_setup()
+    agent = setup_agent(environment_name, log_path)
+    agent.learn(total_timesteps=10000)
+
+
+
+if (__name__ == "__main__"):
+    main()

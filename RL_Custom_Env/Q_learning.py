715613fab51a45859babdec160b711de9edb37a8
Andrew Ha <andrewha123123@gmail.com>
2023-01-27 21:57:37 -0800

    Added 2 function prototypes to Q_learning

--- RL_Custom_Env/Q_learning.py
+++ RL_Custom_Env/Q_learning.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+def Q_learn():
+    """
+    Algorithm for Q-learning
+    """
+    pass
+
+def epsilon_greedy_policy():
+    """
+    epsilon-greedy-policy
+    """
+    pass

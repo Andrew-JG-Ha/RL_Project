a022a32c0a8ad04beafd43dc742b5e7db385a083
Andrew Ha <andrewha123123@gmail.com>
2023-01-28 23:04:37 -0800

    Added starting conditions for Q_learning

--- RL_Custom_Env/Q_learning.py
+++ RL_Custom_Env/Q_learning.py
@@ -4,10 +4,13 @@ def Q_learn():
     """
     Algorithm for Q-learning
     """
+    alpha = 0.5
+    gamma = 0.5
     pass
 
 def epsilon_greedy_policy():
     """
     epsilon-greedy-policy
     """
+    epsilon = 0.5
     pass

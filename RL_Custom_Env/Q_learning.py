386b1b1b7db4e010240990e848c28ebf7f724968
Andrew Ha <andrewha123123@gmail.com>
2023-01-29 20:52:34 -0800

    Added general desciption/format of algorithm for q learning

--- RL_Custom_Env/Q_learning.py
+++ RL_Custom_Env/Q_learning.py
@@ -3,9 +3,11 @@ import numpy as np
 def Q_learn():
     """
     Algorithm for Q-learning
+    Q_update(State, Action) = Q_old(State, Action) + alpha(r_t + gamma*maxQ(State_t+1, Action) - Q_old(State, Action))
     """
     alpha = 0.5
     gamma = 0.5
+
     pass
 
 def epsilon_greedy_policy():

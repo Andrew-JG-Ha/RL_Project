e4299a00a6b29935943884765074aace8c3d284e
Andrew Ha <andrewha123123@gmail.com>
2023-01-31 23:38:15 -0800

    Added Parameters to Q_learning

--- RL_Custom_Env/Q_learning.py
+++ RL_Custom_Env/Q_learning.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def Q_learn():
+def Q_learn(state, action):
     """
     Algorithm for Q-learning
     Q_update(State, Action) = Q_old(State, Action) + alpha(r_t + gamma*maxQ(State_t+1, Action) - Q_old(State, Action))
     """
     alpha = 0.5
     gamma = 0.5
-
-    pass
+    Q_value
+    return Q_value
 
 def epsilon_greedy_policy():
     """
@@ -16,4 +16,4 @@ def epsilon_greedy_policy():
     the function to determine if the agent will explore or exploit previous traning data
     """
     epsilon = 0.5
-    pass
+    

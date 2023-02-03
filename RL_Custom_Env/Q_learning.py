6007342304a2bfe848147076ac013fce1a2aed07
Andrew Ha <andrewha123123@gmail.com>
2023-02-03 01:02:52 -0800

    Added some functions to Q_learning file

--- RL_Custom_Env/Q_learning.py
+++ RL_Custom_Env/Q_learning.py
@@ -1,16 +1,27 @@
 import numpy as np
+import math
+from gym import Env
 
-def Q_learn(state, action):
+def qTrain(environment:type[Env], numberOfEpisodes):
     """
     Algorithm for Q-learning
     Q_update(State, Action) = Q_old(State, Action) + alpha(r_t + gamma*maxQ(State_t+1, Action) - Q_old(State, Action))
     """
     alpha = 0.5
     gamma = 0.5
-    Q_value = Q_old(state, action) + alpha(r_t + gamma*maxarg(state_t+1, action) - Q_old(state, action))
-    return Q_value
+    q_table = np.zeros((pow(environment.observation_space.high, 2), environment.action_space.n))
 
-def epsilon_greedy_policy():
+    for i in range(0, numberOfEpisodes):
+        state = environment.reset()
+
+        done = False
+
+        while not done:
+            qLearn(q_table, )
+
+    return q_table, maxReward
+
+def epsilonGreedyPolicy():
     """
     epsilon-greedy-policy
     the function to determine if the agent will explore or exploit previous traning data
@@ -18,3 +29,22 @@ def epsilon_greedy_policy():
     """
     epsilon = 0.5
     
+def alphaDecay(episodeNumber):
+    """
+    Decay function for alpha value with respect to step number
+    """
+    pass
+
+def gammaDecay(episodeNumber):
+    """
+    Decay function for gamma value with respect to step number
+    """
+    pass
+
+def qLearn(table, episodeNumber):
+    """
+    Function which holds the Q-learning algorithm
+    """
+    alpha = alphaDecay(episodeNumber)
+    gamma = gammaDecay(episodeNumber)
+    epsilonGreedyPolicy()

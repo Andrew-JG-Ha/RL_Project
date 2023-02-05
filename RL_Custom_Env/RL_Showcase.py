6914bb396bed6be9acaf8100e3348721c20b9825
Andrew Ha <andrewha123123@gmail.com>
2023-02-04 22:27:12 -0800

    Added initial pretrained data files

--- RL_Custom_Env/RL_Showcase.py
+++ RL_Custom_Env/RL_Showcase.py
@@ -13,12 +13,29 @@ def main():
     windowsHeight = 720
     textAreaHeight = 80
 
-    emptyMap = generateEmptyMap(fieldSize, windowsWidth, windowsHeight)
-    emptyMapEscape = mazeEscape(fieldSize, windowsWidth, windowsHeight, textAreaHeight, emptyMap)
-    randomMapEscape = mazeEscape(fieldSize, windowsWidth, windowsHeight, textAreaHeight)
+    # emptyMap = generateEmptyMap(fieldSize, windowsWidth, windowsHeight)
+    # emptyMapEscape = mazeEscape(fieldSize, windowsWidth, windowsHeight, textAreaHeight, emptyMap)
+    # randomMapEscape = mazeEscape(fieldSize, windowsWidth, windowsHeight, textAreaHeight)
 
-    qTableEmptyMap = qTrain(emptyMapEscape, 20000)
-    qTableRandomMap = qTrain(randomMapEscape, 20000)
+    # qTableEmptyMap = qTrain(emptyMapEscape, 20000)
+    # qTableRandomMap = qTrain(randomMapEscape, 25000)
+
+    # packageEmptyMap = {"windowsData":(fieldSize, windowsWidth, windowsHeight), "map":(emptyMapEscape.environment.getMap()), "qTable":qTableEmptyMap}
+    # packageRandomMap = {"windowsData":(fieldSize, windowsWidth, windowsHeight), "map":(randomMapEscape.environment.getMap()), "qTable":qTableRandomMap}
+
+
+    # saveToFile(pretrainedPath, "emptyMapPreTrained", packageEmptyMap)
+    # saveToFile(pretrainedPath, "randomMapPreTrained0", packageRandomMap)
+
+    pretrainedPath = pretrainDataDirectory()
+    fileDirectoryEmptyMap = pretrainedPath + "/emptyMapPreTrained.pkl"
+    fileDirectoryRandomMap = pretrainedPath + "/randomMapPreTrained0.pkl"
+
+    with open(fileDirectoryEmptyMap, "rb") as f:
+        pretrainedEmptyMap = pkl.load(f)
+
+    with open(fileDirectoryRandomMap, "rb") as f:
+        pretrainedRandomMap0 = pkl.load(f)
 
 def file_setup():
     """
@@ -45,6 +62,14 @@ def saveToFile(fileDirectory, filename, object):
     pickleFileName = fileDirectory + "/" + filename + '.pkl'
     with open(pickleFileName, 'wb') as f:
         pkl.dump(object, f)
+
+def pretrainDataDirectory():
+    qFolder = "QTables"
+    pretrainedFolder = "pretrainedQTables"
+    currentPath = os.path.abspath(os.path.dirname(__file__))
+    parentPath = os.path.normpath(os.path.join(currentPath, qFolder))
+    pretrainedPath = os.path.normpath(os.path.join(parentPath, pretrainedFolder))
+    return pretrainedPath
 
 if __name__ == "__main__":
     main()

d040c07b1c63885649c08e6e1f0a323c5f87b63e
Andrew Ha <andrewha123123@gmail.com>
2023-02-09 21:20:40 -0800

    DQN agent works, error with DQNAGENT.fit

--- Breakout_Atari_RL/Breakout_Atari_RL.py
+++ Breakout_Atari_RL/Breakout_Atari_RL.py
@@ -1,9 +1,9 @@
 import os
 import gymnasium as gym
 import numpy as np
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Flatten, Conv2D
-from tensorflow.python.keras.optimizers import adam_v2
+from keras.optimizers import Adam
+from keras.layers import Dense, Flatten, Conv2D
+from keras.models import Sequential
 
 from rl.agents import DQNAgent
 from rl.memory import SequentialMemory
@@ -36,15 +36,15 @@ def build_model(height, width, channels, actions):
     model.add(Conv2D(64, (4,4), strides=(2,2), activation="relu")) # convolution neural net with 64 filters of size 4x4
     model.add(Conv2D(64, (3,3), activation="relu")) # convolution neural net with 64 filters of size 3x3
     model.add(Flatten())
-    model.add(Dense(512, activation="relu"))
-    model.add(Dense(256, activation="relu"))
-    model.add(Dense(actions, activation="linear"))
+    model.add(Dense(512, activation="relu", name="dense1"))
+    model.add(Dense(256, activation="relu", name="dense2"))
+    model.add(Dense(actions, activation="linear", name = "dense3"))
     return model
 
 def build_agent(model, actions):
     policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr="eps", value_max=1., value_min=.1, value_test=.2, nb_steps=10000) # Degradation of learning rate, epsilon and discount rate as we get nearer to the goal
     memory = SequentialMemory(limit=1000, window_length=3) # letting the agent read from memory
-    dqn = DQNAgent(model=model, memory=memory, policy=policy, enable_double_dqn=True, dueling_type="avg", nb_actions=actions, nb_steps_warmup=10000)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy, enable_dueling_network=True, dueling_type="avg", nb_actions=actions, nb_steps_warmup=10000)
     return dqn
 
 def main():
@@ -59,8 +59,12 @@ def main():
     actions = environment.action_space.n
 
     model = build_model(height, width, channels, actions)
-
+    
     model.summary()
+
+    dqn = build_agent(model, actions)
+    dqn.compile(Adam(learning_rate=0.0001))
+    dqn.fit(env=environment, nb_steps=10000, visualize=False, verbose=2)
 
     episodes = 5
     for episode in range(1, episodes+1):

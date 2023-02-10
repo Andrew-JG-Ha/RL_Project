9f7508c53aab23c74e4c7663b70ecb51cbd6c0e5
Andrew Ha <andrewha123123@gmail.com>
2023-02-09 20:52:16 -0800

    imported required dependencies for Breakout_Atari_RL, created build_agent and build_model functions

--- Breakout_Atari_RL/Breakout_Atari_RL.py
+++ Breakout_Atari_RL/Breakout_Atari_RL.py
@@ -1,10 +1,14 @@
 import os
 import gymnasium as gym
-from stable_baselines3 import A2C
-from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
-from stable_baselines3.common.vec_env import VecFrameStack
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.env_util import make_atari_env
+import numpy as np
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.python.keras.optimizers import adam_v2
+
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import EpsGreedyQPolicy, LinearAnnealedPolicy
+
 from ale_py.roms import Breakout
 from ale_py import ALEInterface
 
@@ -26,18 +30,37 @@ def file_setup():
         if (not os.path.exists(modelsPath)):
             os.mkdir(modelsPath)
 
+def build_model(height, width, channels, actions):
+    model = Sequential()
+    model.add(Conv2D(32, (8,8), strides=(4,4), activation="relu", input_shape=(3, height, width, channels))) # convolution neural net with 32 filters of size 8x8
+    model.add(Conv2D(64, (4,4), strides=(2,2), activation="relu")) # convolution neural net with 64 filters of size 4x4
+    model.add(Conv2D(64, (3,3), activation="relu")) # convolution neural net with 64 filters of size 3x3
+    model.add(Flatten())
+    model.add(Dense(512, activation="relu"))
+    model.add(Dense(256, activation="relu"))
+    model.add(Dense(actions, activation="linear"))
+    return model
+
+def build_agent(model, actions):
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr="eps", value_max=1., value_min=.1, value_test=.2, nb_steps=10000) # Degradation of learning rate, epsilon and discount rate as we get nearer to the goal
+    memory = SequentialMemory(limit=1000, window_length=3) # letting the agent read from memory
+    dqn = DQNAgent(model=model, memory=memory, policy=policy, enable_double_dqn=True, dueling_type="avg", nb_actions=actions, nb_steps_warmup=10000)
+    return dqn
+
 def main():
     train = True
     testing = True
     file_setup()
 
-    a2c_path = os.path.join(os.path.dirname(__file__), "Training", "Models", "A2C_Breakout_Model")
-
     environment_name = 'ALE/Breakout-v5'
     environment = gym.make(environment_name, render_mode = 'human')
 
-    print(environment.action_space)
-    print(environment.observation_space)
+    height, width, channels = environment.observation_space.shape
+    actions = environment.action_space.n
+
+    model = build_model(height, width, channels, actions)
+
+    model.summary()
 
     episodes = 5
     for episode in range(1, episodes+1):
